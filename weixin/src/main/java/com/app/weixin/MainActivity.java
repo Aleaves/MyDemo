0cc40577827c4b59b47da1e08458ebbd8a4be643
Aleaves <2849459897@qq.com>
2016-03-30 16:25:27 +0800

    wei

---
+++ weixin/src/main/java/com/app/weixin/MainActivity.java
@@ -0,0 +1,157 @@
+package com.app.weixin;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener,ViewPager.OnPageChangeListener {
+
+    private ViewPager mViewPager;
+    private List<Fragment> mTabs = new ArrayList<Fragment>();
+    private String[] mTitles = new String[]
+            { "微信", "通讯录", "发现",
+                    "我" };
+
+    private FragmentPagerAdapter mAdapter;
+
+    private List<ChangeColorIconWithText> mTabIndicators = new ArrayList<ChangeColorIconWithText>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //getActionBar().setDisplayShowHomeEnabled(false);
+        initView();
+        initDatas();
+        mViewPager.setAdapter(mAdapter);
+        initEvent();
+    }
+
+    private void initView(){
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+
+        ChangeColorIconWithText one = (ChangeColorIconWithText) findViewById(R.id.id_indicator_one);
+        mTabIndicators.add(one);
+
+        ChangeColorIconWithText two = (ChangeColorIconWithText) findViewById(R.id.id_indicator_two);
+        mTabIndicators.add(two);
+
+        ChangeColorIconWithText three = (ChangeColorIconWithText) findViewById(R.id.id_indicator_three);
+        mTabIndicators.add(three);
+
+        ChangeColorIconWithText four = (ChangeColorIconWithText) findViewById(R.id.id_indicator_four);
+        mTabIndicators.add(four);
+
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+
+        one.setIconAlpha(1.0f);
+    }
+
+
+    private void initDatas(){
+        for (String title : mTitles)
+        {
+            TabFragment tabFragment = new TabFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(TabFragment.TITLE, title);
+            tabFragment.setArguments(bundle);
+            mTabs.add(tabFragment);
+        }
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
+        {
+
+            @Override
+            public int getCount()
+            {
+                return mTabs.size();
+            }
+
+            @Override
+            public Fragment getItem(int position)
+            {
+                return mTabs.get(position);
+            }
+        };
+    }
+
+    private void initEvent(){
+        mViewPager.setOnPageChangeListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        clickTab(v);
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (positionOffset > 0){
+            ChangeColorIconWithText left = mTabIndicators.get(position);
+            ChangeColorIconWithText right = mTabIndicators.get(position + 1);
+            left.setIconAlpha(1 - positionOffset);
+            right.setIconAlpha(positionOffset);
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    private void clickTab(View v)
+    {
+        resetOtherTabs();
+
+        switch (v.getId())
+        {
+            case R.id.id_indicator_one:
+                mTabIndicators.get(0).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(0, false);
+                break;
+
+            case R.id.id_indicator_two:
+                mTabIndicators.get(1).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(1, false);
+                break;
+
+            case R.id.id_indicator_three:
+                mTabIndicators.get(2).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(2, false);
+                break;
+
+            case R.id.id_indicator_four:
+                mTabIndicators.get(3).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(3, false);
+                break;
+        }
+    }
+
+    /**
+     * 重置其他的TabIndicator的颜色
+     */
+    private void resetOtherTabs() {
+        for (int i = 0; i < mTabIndicators.size(); i++)
+        {
+            mTabIndicators.get(i).setIconAlpha(0);
+        }
+    }
+
+}
